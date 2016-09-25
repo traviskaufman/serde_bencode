776919db16e21ddfb790ffcd348979cf2f75db2a
Travis Kaufman <travis.kaufman@gmail.com>
2016-09-25 13:24:42 -0400

    Inline serialization methods. Don't quote strings

--- src/ser.rs
+++ src/ser.rs
@@ -43,46 +43,57 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
     type StructState = State;
     type StructVariantState = State;
 
+    #[inline]
     fn serialize_bool(&mut self, _: bool) -> Result<()> {
         Err(Error::Syntax(ErrorCode::Custom("Unsupported".to_string())))
     }
 
+    #[inline]
     fn serialize_isize(&mut self, v: isize) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_i8(&mut self, v: i8) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_i16(&mut self, v: i16) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_i32(&mut self, v: i32) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_i64(&mut self, v: i64) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_usize(&mut self, v: usize) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_u8(&mut self, v: u8) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_u16(&mut self, v: u16) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_u32(&mut self, v: u32) -> Result<()> {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_u64(&mut self, v: u64) -> Result<()> {
         if v > i64::max_value() as u64 {
             return Err(Error::Syntax(ErrorCode::Custom("Unsupported integer value".to_string())));
@@ -90,24 +101,27 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         bencode_int!(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_f32(&mut self, v: f32) -> Result<()> {
         bencode_int!(&mut self.writer, v as i64)
     }
 
+    #[inline]
     fn serialize_f64(&mut self, v: f64) -> Result<()> {
         bencode_int!(&mut self.writer, v as i64)
     }
 
-    // TODO: Escape strs
-
+    #[inline]
     fn serialize_char(&mut self, v: char) -> Result<()> {
         self.formatter.string(&mut self.writer, &char::to_string(&v))
     }
 
+    #[inline]
     fn serialize_str(&mut self, v: &str) -> Result<()> {
         self.formatter.string(&mut self.writer, v)
     }
 
+    #[inline]
     fn serialize_bytes(&mut self, v: &[u8]) -> Result<()> {
         let mut state = try!(self.serialize_seq(Some(v.len())));
         for byte in v {
@@ -116,22 +130,27 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         self.serialize_seq_end(state)
     }
 
+    #[inline]
     fn serialize_unit(&mut self) -> Result<()> {
         self.serialize_str("")
     }
 
+    #[inline]
     fn serialize_unit_struct(&mut self, _name: &'static str) -> Result<()> {
         self.serialize_unit()
     }
 
+    #[inline]
     fn serialize_unit_variant(&mut self, _name: &'static str, _variant_index: usize, _variant: &'static str) -> Result<()> {
         self.serialize_unit()
     }
 
+    #[inline]
     fn serialize_newtype_struct<T: ser::Serialize>(&mut self, _name: &'static str, value: T) -> Result<()> {
         value.serialize(self)
     }
 
+    #[inline]
     fn serialize_newtype_variant<T: ser::Serialize>(
             &mut self, _name: &'static str, _variant_index: usize, variant: &'static str, value: T) -> Result<()> {
         try!(self.formatter.dict_open(&mut self.writer));
@@ -140,14 +159,17 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         self.formatter.dict_close(&mut self.writer)
     }
 
+    #[inline]
     fn serialize_none(&mut self) -> Result<()> {
         self.serialize_unit()
     }
 
+    #[inline]
     fn serialize_some<T: ser::Serialize>(&mut self, value: T) -> Result<()> {
         value.serialize(self)
     }
 
+    #[inline]
     fn serialize_seq(&mut self, len: Option<usize>) -> Result<State> {
         try!(self.formatter.list_open(&mut self.writer));
         if len == Some(0) {
@@ -157,11 +179,13 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         Ok(State::First)
     }
 
+    #[inline]
     fn serialize_seq_elt<T: ser::Serialize>(&mut self, state: &mut State, value: T) -> Result<()> {
         *state = State::Rest;
         value.serialize(self)
     }
 
+    #[inline]
     fn serialize_seq_end(&mut self, state: State) -> Result<()> {
         match state {
             State::Empty => Ok(()),
@@ -169,34 +193,42 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         }
     }
 
+    #[inline]
     fn serialize_seq_fixed_size(&mut self, size: usize) -> Result<State> {
         self.serialize_seq(Some(size))
     }
 
+    #[inline]
     fn serialize_tuple(&mut self, size: usize) -> Result<State> {
         self.serialize_seq(Some(size))
     }
 
+    #[inline]
     fn serialize_tuple_elt<T: ser::Serialize>(&mut self, state: &mut State, value: T) -> Result<()> {
         self.serialize_seq_elt(state, value)
     }
 
+    #[inline]
     fn serialize_tuple_end(&mut self, state: State) -> Result<()> {
         self.serialize_seq_end(state)
     }
 
+    #[inline]
     fn serialize_tuple_struct(&mut self, _name: &'static str, size: usize) -> Result<State> {
         self.serialize_seq(Some(size))
     }
 
+    #[inline]
     fn serialize_tuple_struct_elt<T: ser::Serialize>(&mut self, state: &mut State, value: T) -> Result<()> {
         self.serialize_seq_elt(state, value)
     }
 
+    #[inline]
     fn serialize_tuple_struct_end(&mut self, state: State) -> Result<()> {
         self.serialize_seq_end(state)
     }
 
+    #[inline]
     fn serialize_tuple_variant(
         &mut self,
         _name: &'static str,
@@ -265,6 +297,7 @@ impl<W> ser::Serializer for Serializer<W> where W: io::Write {
         }
     }
 
+    #[inline]
     fn serialize_struct(
         &mut self,
         _name: &'static str,
@@ -332,7 +365,7 @@ pub struct Formatter;
 
 impl Formatter {
     pub fn string<W>(&self, w: &mut W, s: &str) -> Result<()> where W: io::Write {
-        write!(w, "{}:\"{}\"", s.len(), s).map_err(From::from)
+        write!(w, "{}:{}", s.len(), s).map_err(From::from)
     }
 
     pub fn dict_open<W>(&self, w: &mut W) -> Result<()> where W: io::Write {
