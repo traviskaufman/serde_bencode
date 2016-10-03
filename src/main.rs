e511417acbe18060418ef3d2a27f8790dd0a9889
Travis Kaufman <travis.kaufman@gmail.com>
2016-10-02 23:55:40 -0400

    Print original to compare with deserialized

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         i: 42,
         v: vec![Point{ x: 1, y: 2}, Point{ x: 4, y: 7}, Point{ x: 8, y: 19 }]
     };
+    println!("original = {:?}", c);
     let serialized = bencode::to_string(&c).unwrap();
     println!("serialized = {}", serialized);
     let deserialized: Complex = bencode::from_string(serialized).unwrap();
